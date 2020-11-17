15f3227231a086b8788189fef12ae94d9f3aecce
Areeb Bhaila <areeb.bhaila@gmail.com>
2020-11-17 14:35:01 +0000

    fixed the routes issue with user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cars do
     resources :bookings
