015f34fa3f174a51f844ff5d2cd1e6d6605674fe
Areeb Bhaila <areeb.bhaila@gmail.com>
2020-11-17 13:15:57 +0000

    added booking controller - nested routes in cars NOT users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users do
-    resources :bookings
-  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cars
+  resources :cars do
+    resources :bookings
+  end
   resources :bookings, only: [:destroy]
 end
