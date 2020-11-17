cd19a1dbb7eda8d9372d034dc46dc24d7cf5fcad
Jose Benitez <pepebenitez12@gmail.com>
2020-11-17 15:13:00 +0000

    Added Validations for Cars

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,11 @@
 class Car < ApplicationRecord
   belongs_to :user
+  # price, year, model, brand, description
+  # What do we need??
+  validates :year, inclusion: { in: 1910..2020, message: "%{value} is not a valid year" }
+  validates :year, presence: true
+  validates :price, presence: true
+  validates :model, presence: true
+  validates :brand, presence: true
+  validates :description, presence: true
 end
