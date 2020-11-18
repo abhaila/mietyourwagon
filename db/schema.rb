dead7fed52bc4921464c8f735a36b04261fb5d8f
Benni Matzen <benniafmatzen@web.de>
2020-11-18 13:11:55 +0100

    created a model map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_153341) do
+ActiveRecord::Schema.define(version: 2020_11_18_120824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,12 @@ ActiveRecord::Schema.define(version: 2020_11_17_153341) do
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "price", precision: 8, scale: 2
     t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "maps", force: :cascade do |t|
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
