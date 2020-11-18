ba4544896e898536afc6028b84cacc444b77d7a6
Jose Benitez <pepebenitez12@gmail.com>
2020-11-18 13:21:15 +0000

    Cleaned data base, created cars, users and bookings (without images)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Cleaning database...."
+Booking.destroy_all
+Car.destroy_all
+User.destroy_all
 
-car1 = Car.new(brand: "Ferrari", model: "F8", year: 2020)
-car2 = Car.new(brand: "Ford", model: "Mustang", year: 1967)
-car3 = Car.new(brand: "Toyota", model: "Hilux", year: 1993)
+puts "Creating users..."
 
-car1.save!
-car2.save
-car3.save
+User.create!(email: "a@lewagon.com" , password: "secret", first_name: "Areeb", last_name: "Bhaila", address: "London")
+User.create!(email: "b@lewagon.com" , password: "secret", first_name: "Benni", last_name: "Matzen", address: "Hamburg"))
+User.create!(email: "c@lewagon.com" , password: "secret", first_name: "Pepe", last_name: "Benitez", address: "London"))
+
+puts "Creating cars..."
+Car.create!(user: User.all.sample, brand: "Ferrari", model: "F8", year: 2020, price: 500, description: "Hot and Shiny", address: "London")
+Car.create!(user: User.all.sample, brand: "Ford", model: "Mustang", year: 1967, price: 420, description: "Her name is Eleonor", address: "London")
+Car.create!(user: User.all.sample, brand: "Toyota", model: "Hilux", year: 1993, price: 20, description: "Indestructible", address: "London")
+
+puts "Creating bookings..."
+Boooking.create!(user: User.all.sample, car: Car.all.sample, start_date: "2020-11-19 12:18:00 UTC", end_date: "2020-11-25 12:18:00 UTC")
+Booking.create!(user: User.all.sample, car: Car.all.sample)
+Booking.create!(user: User.all.sample, car: Car.all.sample)
+
+puts "Done! #{User.count} users created."
+puts "Done! #{Car.count} cars created."
+puts "Done! #{Booking.count} bookings created."
+
+
+
+
